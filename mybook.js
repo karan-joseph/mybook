7a664d238f6eacd4f17bbfeef277f7b1ae28f955
karan <karanjarmy@gmail.com>
2025-12-29 22:48:26 +0530

    new book

---
+++ mybook.js
@@ -0,0 +1,32 @@
+
+var popoverley=document.querySelector(".pop_overley")
+var poppage=document.querySelector(".pop_page")
+var addbutton=document.querySelector(".add_button")
+addbutton.addEventListener("click",function(){
+    popoverley.style.display="block"
+    poppage.style.display="block"
+})
+var cancelbook=document.getElementById("cancel_book")
+cancelbook.addEventListener("click",function(event){
+    event.preventDefault()
+  popoverley.style.display="none"
+    poppage.style.display="none"  
+})
+var holebook=document.querySelector(".hole_book")
+var addbook=document.getElementById("add_book")
+var booknameid=document.getElementById("book_name_id")
+var bookaurtornameid=document.getElementById("book_aurtogit config --global user.namer_name_id")
+var discripionid=document.getElementById("discripion_id")
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+  var div=document.createElement("div") 
+  div.setAttribute("class","book_page")
+  div.innerHTML=`<h2>${booknameid.value}</h2>
+    <h5>${bookaurtornameid.value}</h5>
+    <p>${discripionid.value}</p>
+    <button onclick="this.parentElement.remove()">delete</button>`
+  holebook.append(div)
+  popoverley.style.display="none"
+    poppage.style.display="none" 
+  })
+  
